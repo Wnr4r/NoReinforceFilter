39b423069d52830f5b3535054ef123b21d79ed7e
youngolutosin <45429273+youngolutosin@users.noreply.github.com>
2022-04-22 13:38:23 +0300

    Update Program.cs
    Crab faction and accurate reinforcement history query function implemented using the new lending API. User prompt to specify the miners to be looted based on a last reinforcement transaction time done was also implemented.

--- CrabadaFilter/Program.cs
+++ CrabadaFilter/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -12,29 +12,46 @@ namespace CrabadaFilter
     {
         static void Main(string[] args)
         {
+
             string response = string.Empty;
             do
             {
                 Console.Write("Enter starting mine ID: ");
                 int startMineID = Int32.Parse(Console.ReadLine());
+
                 Console.Write("Enter how many additional mines to scan: ");
                 int numberOfMines = Int32.Parse(Console.ReadLine());
-                //int numberOfMines = 5;
-                //int startMineID = 4840793;
+                
+                Console.Write("Enter last reinforcement time (Hours) threshold to query reinforcement history: ");
+                double minReinforcemnentTransTimeHr = Double.Parse(Console.ReadLine());
+                
                 int stopMineID = startMineID + numberOfMines;
 
                 for (int i = startMineID; i <= stopMineID; i++)
                 {
-                    string address = filterOwnerAddress(i);
-                    //if address is empty or miner has own crab for reinforcing, continue to next iteration
-                    if (string.IsNullOrWhiteSpace(address) || isCrabAvailable(address)) continue;
-                    int totalRecord = filterNoReinforceAddress(address);
-                    if (totalRecord == 0)
+                    
+                    try
                     {
-                        Console.WriteLine($"MineID: {i} \t OwnerAdress: {address}");
-                    }
+                        string address = filterOwnerAddress(i);
+                        string crabFaction = minerFaction(i);
+                        double lastReinforceTimeDiffHHour = filterNoReinforceAddress(address);
+                        
+                        //if address is empty or miner has own crab for reinforcing, continue to next iteration
+                        if (string.IsNullOrWhiteSpace(address) || isCrabAvailable(address)) continue;
+                        
+                        if (lastReinforceTimeDiffHHour >= minReinforcemnentTransTimeHr)
+                        {
+                                Console.WriteLine($"CrabFaction: {crabFaction} \t MineID: {i} \t OwnerAdress: {address} \t LastReinforceTime:  {lastReinforceTimeDiffHHour} Hrs");
+                        } 
+                    } 
 
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Error encountered");
+                    } 
+                    
                 }
+
                 Console.WriteLine("\n Completed!!!!");
                 Console.Write("\n Do you wish to check another mine ID series? Type yes to continue: ");
                 response = Console.ReadLine();
@@ -76,7 +93,7 @@ namespace CrabadaFilter
         /// </summary>
         /// <param name="address">Wallet address.</param>
         /// <returns>reinforcement history of the input address. 0 - means, no reinforement history</returns>
-        public static int filterNoReinforceAddress(string address)
+        public static double filterNoReinforceAddress(string address)
         {
             //check to see that address returned is valid
             if (address.Length != 42)
@@ -85,13 +102,20 @@ namespace CrabadaFilter
             }
             //sleep to avoid ban
             Thread.Sleep(1000);
-            string url = $"https://idle-api.crabada.com/public/idle/crabadas/lending?borrower_address={address}&limit=100";
+            DateTime currentDate = DateTime.Now;
+            string url = $"https://idle-api.crabada.com/public/idle/crabadas/lending/history?borrower_address={address}&orderBy=transaction_time&order=desc&limit=2";
             var client = new WebClient();
             client.Headers.Add("User-Agent: Other");
             var content = client.DownloadString(url);
             dynamic stuff = JObject.Parse(content);
-            int totalRecord = stuff.result.totalRecord;
-            return totalRecord;
+
+            double lastReinforcementTime = stuff.result.data[0].transaction_time;
+            DateTime lastReinforcementTimeInHRF  = new DateTime(1970, 1, 1, 0, 0, 0, 0); //from start epoch time in HRF:Human Readable Forrmat
+            lastReinforcementTimeInHRF = lastReinforcementTimeInHRF.AddSeconds(lastReinforcementTime); // update reinforcement using the latest tran_time
+            double lastTransacTimeDiffHr =  Math.Round(((currentDate - lastReinforcementTimeInHRF).TotalDays) * 24);
+            
+            // calibrate epoch to HRF conversion
+            return lastTransacTimeDiffHr - 3;
         }
 
         /// <summary>
@@ -124,6 +148,25 @@ namespace CrabadaFilter
         }
 
 
+        /// <summary>
+        /// Checks miner's crab faction
+        /// </summary>
+        /// <param name="mineID">Miner's ID</param>
+        /// <returns>Miner's crab faction</returns>
+        public static string minerFaction(int mineID)
+        {
+        
+            Thread.Sleep(1000);
+            string url = $"https://idle-api.crabada.com/public/idle/mine/{mineID}";
+            var client = new WebClient();
+            client.Headers.Add("User-Agent: Other");
+            var content = client.DownloadString(url);
+            dynamic stuff = JObject.Parse(content);
+            string teamFaction = stuff.result.defense_team_faction;
+
+            return teamFaction;
+            
+        }
 
         //not used for now
         public static string queryAPI(string url )
